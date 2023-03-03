c65e6a55967ab3de21a84a85caf3738dd4a27eff
mephisto <pikalov.ea97@gmail.com>
2023-03-03 22:26:58 +0300

    gjghj

--- src/Manual/Manual.jsx
+++ src/Manual/Manual.jsx
@@ -8,37 +8,43 @@ function Manual({manualProps,setManualProps}){
             </div>
             <strong>Инструкция вычисления </strong>
         <div className="div-border">
-            <div className="div-margin-top">Условный размер 1-го типа<br/><strong>3x70(5)</strong>
+            <div className="div-margin-top">Условный размер 1-го типа(Основная)<br/><strong>3x325(1)</strong>
+            </div>
+            <img src="/img/type7.png"/>
+           <span className="span-margin-top">Корень = 85</span> 
+        </div>
+        <div className="div-border">
+            <div className="div-margin-top">Условный размер 2-го типа(Основная)<br/><strong>3x70(5)</strong>
             </div>
             <img src="/img/type1.png"/>
            <span className="span-margin-top">Корень = 66.5</span> 
         </div>
         <div className="div-border">
-            <div className="div-margin-top">Условный размер 2-го типа(Горбатка)<br/><strong>3x100(1)+120(1)</strong>
+            <div className="div-margin-top">Условный размер 3-го типа(Горбатка)<br/><strong>3x100(1)+120(1)</strong>
             </div>
             <img src="/img/type2.png"/>
             <span className="span-margin-top">Корень = 136</span>
         </div>
         <div className="div-border">
-            <div className="div-margin-top">Условный размер 3-го типа(Горбатка)<br/><strong>3x100(1)+115(1)+90(1)</strong>
+            <div className="div-margin-top">Условный размер 4-го типа(Горбатка)<br/><strong>3x100(1)+115(1)+90(1)</strong>
             </div>
             <img src="/img/type3.png"/>
             <span className="span-margin-top">Корень = 92</span>
         </div>
         <div className="div-border">
-            <div className="div-margin-top">Условный размер 4-го типа(Горбатка)<br/><strong>3x115(1)+120(2)</strong>
+            <div className="div-margin-top">Условный размер 5-го типа(Горбатка)<br/><strong>3x115(1)+120(2)</strong>
             </div>
             <img src="/img/type4.png"/>
             <span className="span-margin-top">Корень = 67</span>
         </div>
         <div className="div-border">
-            <div className="div-margin-top">Условный размер 5-го типа(Горбатка)<br/><strong>3x36(6)+40(5)</strong>
+            <div className="div-margin-top">Условный размер 6-го типа(Горбатка)<br/><strong>3x36(6)+40(5)</strong>
             </div>
             <img src="/img/type5.png"/>
             <span className="span-margin-top">Корень = 26</span>
         </div>
         <div className="div-border">
-            <div className="div-margin-top">Условный размер 6-го типа(Горбатка)<br/><strong>3x25(5)+100(1)</strong>
+            <div className="div-margin-top">Условный размер 7-го типа(Горбатка)<br/><strong>3x25(5)+100(1)</strong>
             </div>
             <img src="/img/type6.png"/>
             <span className="span-margin-top">Корень = 127.5</span>
